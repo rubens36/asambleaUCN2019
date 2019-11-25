fadb7276c5d043187dc3a6cad51ee2abfbdc7c4c
rubens36 <lic.rubensanchezacosta@gmail.com>
2019-11-25 11:29:48 -0300

    Word cloud primer commit

--- word_cloud.py
+++ word_cloud.py
@@ -1,7 +1,6 @@
 # Start with loading all necessary libraries
 import numpy as np
 import pandas as pd
-from os import path
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from pathlib import Path
